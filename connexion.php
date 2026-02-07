1857035c16e2eb2cf1caa6b598e7576605b8fcd6
touboul <touboulelie22@gmail.com>
2026-02-08 00:46:10 +0100

    page php

--- connexion.php
+++ connexion.php
@@ -2,8 +2,8 @@
  
 //Connexion à la base de données
 function connectToDB() {
-    $dsn = "mysql:host=db.3wa.io;port=3306;dbname=lukatrehout_gestionnaire_taches;charset=utf8";
-    $db = new PDO($dsn,"lukatrehout", "872c9df53a4d151ccc68bc096323ab7b");
+    $dsn = "mysql:host=db.3wa.io;port=3306;dbname=elietouboul_gestionaire;charset=utf8";
+    $db = new PDO($dsn,"elietouboul", "35b0c636b367ef40b68ce5c495547588");
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
     return $db;
 }
