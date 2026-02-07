64389efb8979af3d9e7df29249c165c8e5dd9d28
touboul <touboulelie22@gmail.com>
2026-02-07 15:24:42 +0100

    Connexion à la base de données, fonctionnalité du formulaire de la page register

--- connexion.php
+++ connexion.php
@@ -1,8 +1,9 @@
 <?php
-
+ 
 //Connexion à la base de données
 function connectToDB() {
     $dsn = "mysql:host=db.3wa.io;port=3306;dbname=lukatrehout_gestionnaire_taches;charset=utf8";
-    $db = new PDO($dsn,"root", "");
+    $db = new PDO($dsn,"lukatrehout", "872c9df53a4d151ccc68bc096323ab7b");
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
     return $db;
 }
