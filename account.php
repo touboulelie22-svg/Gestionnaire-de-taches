89bb8c2b5a16022ba3ab81a53b743c8f3ec5348c
touboul <touboulelie22@gmail.com>
2026-02-08 18:52:51 +0100

    ajout php account php

--- account.php
+++ account.php
@@ -1,2 +1,23 @@
 <?php
+ 
+include ('connexion.php');
+ 
+if (!empty($_POST)){
+    $db = connectToDB();
+    $title = $_POST['title'];
+    $content = $_POST['content'];
+ 
+$requete = $db->prepare("INSERT INTO tasks (title,content,urgent,important) VALUES (:title, :content, :urgent, :important)");
+ 
+$requete->execute([
+    ':title' => $title,
+    ':content' => $content,
+    ':urgent' => 0,
+    ':important' => 0
+    ]);
+}
+else {
+    $requête = $db->prepare("SELECT * FROM tasks");
+    $tasks = $requete->fetchAll(PDO::FETCH_ASSOC);
+}
 include ('account.phtml');
